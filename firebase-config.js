96b520b0ac03443d2167b69e75de6b1a2e7c97ed
snexis <sonu26bolg@gmail.com>
2026-07-13 13:29:10 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,16 +4,18 @@
  * এটি ভুলেও গিটহাবে (GitHub) পুশ বা আপলোড করবেন না।
  */
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBOHO_fnREBfPjykd7Lo_ZBsAXy3keoZW4",
+  apiKey: "AIzaSyB0HO_fnRt3FMjykq7Lo_Z0sAYy3kee2W4",
   authDomain: "a-toz-patti.firebaseapp.com",
   databaseURL: "https://a-toz-patti-default-rtdb.firebaseio.com",
   projectId: "a-toz-patti",
   storageBucket: "a-toz-patti.firebasestorage.app",
   messagingSenderId: "71546188781",
-  appId: "1:71546188781:web:d9bfea657335132c66de4"
+  appId: "1:71546188781:web:19bfea6537335132c86de4",
+  measurementId: "G-8ENRFZRWLP"
 };
-
+window.firebaseConfig = firebaseConfig;
 // গ্লোবাল উইন্ডো অবজেক্টে কনফিগারেশন এক্সপোজ করা
 // এর ফলে আমাদের প্রোজেক্টের app.js বা অন্য যেকোনো স্ক্রিপ্ট সরাসরি এই কানেকশনটি ব্যবহার করতে পারবে
 window.firebaseConfig = firebaseConfig;
